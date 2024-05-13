5dab547bd7c1f0e813445483b95fa9f8bcd4aa7f
renanrod4 <renanrdemeneses@gmail.com>
2024-05-13 13:17:07 -0300

    Local jsons + new license
    Special thanks to opencagedata.com for allowing its jsons to be used locally allowing for more requests and faster loading speed;
    The new license in use is the creative commons license non comercial 4.0

---
+++ js/getCords.js
@@ -0,0 +1,42 @@
+var answerCordinates;
+
+function getCountryCoordinates(countryName, callback) {
+    let abrCountryName;
+    countrynames.forEach((i) => {
+        if (clearstr(i) == clearstr(countryName)) {
+            abrCountryName = abr[countrynames.indexOf(countryName)];
+        }
+    });
+
+    var jsonUrl = document.location.href+"cords/" + clearstr(abrCountryName) + ".json";
+    var request = new XMLHttpRequest();
+    request.open("GET", jsonUrl, true);
+
+    request.onload = function () {
+        if (request.status === 200) {
+            var data = JSON.parse(request.responseText);
+            var coordinates = null;
+
+            if (data.results && data.results.length > 0 && data.results[0].geometry) {
+                coordinates = data.results[0].geometry;
+            }
+            callback(null, coordinates);
+        } else {
+            callback("Failed to load JSON file", null);
+        }
+    };
+
+    request.onerror = function () {
+        callback("Unable to connect to server", null);
+    };
+
+    request.send();
+}
+
+getCountryCoordinates(countrynames[randomnum], function (error, coordinates) {
+    if (error) {
+        console.error("Error:", error);
+    } else {
+        answerCordinates = coordinates;
+    }
+});
---
+++ js/input.js
@@ -0,0 +1,64 @@
+
+datalist.style.top = input.offsetTop + 50 + "px";
+
+input.addEventListener("click", () => {
+    input.value = "";
+    if (won || gameover){
+
+        datalist.classList.remove("active");
+        datalist.style.display = "none!important";
+    }else{
+        datalist.classList.toggle("active");
+    }
+});
+function clearstr(str) {
+    str = str.toLowerCase();
+
+    str = str.replace("á", "a").replace("â", "a").replace("ã","a").replace("à","a").replace("ä","a");
+    str = str.replace("é", "e").replace("ê", "e").replace("ẽ","e").replace("è","e").replace("ë","e");
+    str = str.replace("í", "i").replace("î", "i").replace("ĩ","i").replace("ì","i").replace("ï","i");
+    str = str.replace("ó", "o").replace("ô", "o").replace("õ","o").replace("ò","o").replace("ö","o");
+    str = str.replace("ú", "u").replace("û", "u").replace("ũ","u").replace("ù","u").replace("ü","u");
+    str = str.replace("ç","c").replace(" ", "-");
+    str = str.replace(/[^a-zA-Z ]/g, "")
+
+    return str;
+}
+document.addEventListener("keydown",(a)=>{
+    if(!datalist.classList.contains("active")){
+        input.click();
+        input.focus();
+        input.autofocus = true
+    }
+    
+})
+input.addEventListener("keyup", (a) => {
+    var filter, li, j, textvalue;
+    filter = clearstr(input.value);
+    li = document.querySelectorAll("li");
+    j = 0;
+
+    for (let i = 0; i < li.length; i++) {
+        var liCount = li[i];
+        textvalue = liCount.textContent || liCount.innerText;
+        if (clearstr(textvalue).indexOf(filter) > -1) {
+            li[i].style.display = "";
+            if (a.key == "Enter" && j == 0) {
+                input.value = li[i].innerText;
+                j++;
+                verify();
+                input.click();
+            }
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+});
+
+dataelement.forEach((optionListSingle) => {
+    optionListSingle.addEventListener("click", () => {
+        var text = optionListSingle.innerText;
+        input.value = text;
+        datalist.classList.remove("active");
+    });
+});
